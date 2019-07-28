f9b0dcae61247fd37c014ebdaf29dd34a9a28916
Valen0602 <45152685+valen0602@users.noreply.github.com>
2019-07-28 14:31:39 +0700

    Add files via upload

---
+++ cmatrix.sh
@@ -0,0 +1,21 @@
+clear
+
+figlet Valen0602 | lolcat
+ 
+echo "TUNGGU SEBENTAR" | lolcat
+
+echo "MENUNGGU ITU MENYENANGKAN" | lolcat
+
+pkg install cmatrix
+
+echo "OKE SIAP" | lolcat
+
+echo "--- 3 ---" | lolcat
+
+echo "--- 2 ---" | lolcat
+
+echo "--- 1 ---" | lolcat
+
+echo "GO" | lolcat
+
+cmatrix
